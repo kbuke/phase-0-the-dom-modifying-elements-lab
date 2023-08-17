254f6223ad0093c3491a19a6625e2c78a1dc56c9
kaan <kabuke13@gmail.com>
2023-08-17 15:32:24 +0100

    lab changes

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // Write your code here!
-main.remove()
+// main.remove()
 
-const newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory");
+// const newHeader = document.createElement("h1");
+// newHeader.setAttribute("id", "victory");
 
-newHeader.textContent = "Kaan is the champion"
+// newHeader.textContent = "Kaan is the champion"
+
+const remElement = main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+
+newHeader.innerHTML = 
+"<p>Kaan Buke is the champion</p>"
+// console.log(newHeader)
+
+
+// document.body.append(newHeader)
